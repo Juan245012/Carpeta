a8fca6f152c3478839b20da620c4b3f2dadff841
Juan245012 <165919494+Juan245012@users.noreply.github.com>
2024-05-01 22:56:45 -0500

    Add files via upload

---
+++ 1 de mayo/Python/ejercito_3.py
@@ -0,0 +1,44 @@
+num = float(input("por favor ingresar el numero deseado para comprobar si esta en la tabla de Fibonacci (hasta el 377) "))
+
+fibouno = 1 + 1
+fibdos = 1 + 2
+fibtres = 2 + 3
+fibcuatro = 3 + 5
+fibcinco = 5 + 8
+fibseis = 8 + 13
+fibsiete = 13 + 21
+fibocho = 21 + 34
+fibnueve = 21 + 34
+fibdiez = 34 + 55
+fibonce= 55 + 89
+fibdoce = 89 + 144
+fibotrece = 144 + 233
+
+if num == fibouno:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibdos:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibtres:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibcuatro:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibcinco:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibseis:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibsiete:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibocho:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibnueve:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibdiez:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibonce:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibdoce:
+    print(f"esta en la tabla Fibonacci")
+elif num == fibotrece:
+    print(f"esta en la tabla Fibonacci")
+else:
+    print("no esta en la tabla Fibonacci")
---
+++ 1 de mayo/Seudocodigo/Ejercicio_3.psc
@@ -0,0 +1,20 @@
+Algoritmo Ejercicio_3
+	fibouno <- 1+1
+	fibdos <- 1+2
+	fibtres <- 2+3
+	fibcuatro <- 3+5
+	fibcinco <- 5+8
+	fibseis <- 8+13
+	fibsiete <- 13+21
+	fibocho <- 21+34
+	fibnueve <- 21+34
+	fibdiez <- 34+55
+	fibonce <- 55+89
+	fibdoce <- 89+144
+	fibotrece <- 144+233
+	Si num==fib1 O num==fib2 O num==fib3 O num==fib4 O num==fib5 O num==fib6 O num==fib7 O num==fib8 O num==fib9 O num==fib10 O num==fib11 O num==fib12 O num==fib13 O num==fib14 Entonces
+		Escribir ('El número está en la serie de Fibonacci.')
+	SiNo
+		Escribir ('El número no está en la serie de Fibonacci.')
+	FinSi
+FinAlgoritmo
